1dcd3f51eb95fdcb5a7d5c1f108bf5beae61b2b0
jacquesboitreaud <jacquesboitreaud@gmail.com>
2020-01-10 11:07:01 -0500

    exp1.sh

--- exp1.sh
+++ exp1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python3 main_dock.py -e 2
-python3 main_dock.py -e 4
-python3 main_dock.py -e 8
-python3 main_dock.py -e 16
-python3 main_dock.py -e 32
-python3 main_dock.py -e 64
+python3 main_dock.py -e 2 -o decoys
+python3 main_dock.py -e 4 -o decoys
+python3 main_dock.py -e 8 -o decoys
+python3 main_dock.py -e 16 -o decoys
+python3 main_dock.py -e 32 -o decoys
+python3 main_dock.py -e 64 -o decoys
