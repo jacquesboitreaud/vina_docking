f53e5a0625c5087e6bb99709ec40896c1cffc506
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-02-06 09:33:16 -0500

    cleaned up

--- experiments_scripts/enrichment_over_sample.py
+++ experiments_scripts/enrichment_over_sample.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-df = pd.read_csv('../data/scored/egfr_dude_scored_64.csv')
+df = pd.read_csv('../data/scored/abl1_dude_scored.csv')
 
 actives = df[df['active']==1]
 decoys = df[df['decoy']==1]
