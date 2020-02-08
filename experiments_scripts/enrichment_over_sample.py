080ee5a77ff461c513756e45f2b5c8d633f1c8d4
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-02-08 13:32:37 -0500

    added moses ligands

--- experiments_scripts/enrichment_over_sample.py
+++ experiments_scripts/enrichment_over_sample.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-df = pd.read_csv('../data/scored/abl1_dude_scored.csv')
+df = pd.read_csv('../data/scored/drd3_dude_scored.csv')
 
 actives = df[df['active']==1]
 decoys = df[df['decoy']==1]
---
+++ make_dataframe_moses.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 16 11:16:21 2020
+
+@author: jacqu
+
+Sample random moses molecules in a dataframe with SMILES for docking
+"""
+
+import pandas as pd 
+import numpy as np
+
+import os 
+
+df = pd.read_csv('../graph2smiles/data/moses_train.csv')
+
+dftest = pd.read_csv('../graph2smiles/data/moses_test.csv')
+
+# Sampling 
+
+rd = df.sample(50000)
+rd = pd.concat([rd,dftest.sample(10000)])
+
+# Save 
+rd.to_csv(f'C:/Users/jacqu/Documents/GitHub/vina_docking/data/ligands/moses_sample.csv')
