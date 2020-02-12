60b7195a19cd68004107d7dee20fb1155e319a28
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-02-12 16:10:05 -0500

    debug

--- main_dock_smi.py
+++ main_dock_smi.py
@@ -10,6 +10,8 @@ File to run vina docking on a dataframe with smiles strings
     Path to receptor PDB file 
     exhaustiveness 
     Suffix for scores file output 
+    
+(Paths for compute canada server) 
 
 """
 
@@ -39,15 +41,17 @@ def cline():
 def main(args):
     # Runs the docking process with the args provided
     
-    # Copy receptor file from the DUDE dir if first time using this target. 
-    shutil.copyfile(f'/home/mcb/users/jboitr/data/all/{args.target}/receptor.pdb',f'data/receptors/{args.target}.pdb')
+    home_dir='/home/jboitr/projects/def-jeromew/jboitr'
+    
+    # Uncomment to Copy receptor file from the DUDE dir if first time using this target. 
+    #shutil.copyfile(f'/home/mcb/users/jboitr/data/all/{args.target}/receptor.pdb',f'data/receptors/{args.target}.pdb')
     
     receptor_filepath = f'data/receptors/{args.target}.pdb'
     
     # target to pdbqt 
     subprocess.run(['python3','pdb_select.py',f'{receptor_filepath}','! hydro', f'{receptor_filepath}'])
-    subprocess.run(['/home/mcb/users/jboitr/mgltools_x86_64Linux2_1.5.6/bin/pythonsh', 'prepare_receptor4.py',
-                    f'-r /home/mcb/users/jboitr/vina_docking/{receptor_filepath}','-o tmp/receptor.pdbqt', '-A hydrogens'])
+    subprocess.run([f'{home_dir}/local/mgltools_x86_64Linux2_1.5.6/bin/pythonsh', 'prepare_receptor4.py',
+                    f'-r {home_dir}/vina_docking/{receptor_filepath}','-o tmp/receptor.pdbqt', '-A hydrogens'])
     
     # Iterate on molecules
     mols_df = pd.read_csv(f'data/ligands/{args.dataframe}.csv')
@@ -67,13 +71,13 @@ def main(args):
             f.close()
         
         # ligand mol2 to pdbqt 
-        subprocess.run(['/home/mcb/users/jboitr/mgltools_x86_64Linux2_1.5.6/bin/pythonsh', 'prepare_ligand4.py',
+        subprocess.run([f'{home_dir}/local/mgltools_x86_64Linux2_1.5.6/bin/pythonsh', 'prepare_ligand4.py',
                         f'-l tmp/ligand.mol2', '-o tmp/ligand.pdbqt', '-A hydrogens'])
         
         # RUN DOCKING 
         start=time()
-        subprocess.run(['/home/mcb/users/jboitr/local/autodock_vina_1_1_2_linux_x86/bin/vina',
-                    '--config', f'/home/mcb/users/jboitr/vina_docking/data/conf/conf_{args.target}.txt','--exhaustiveness', f'{args.ex}', 
+        subprocess.run([f'{home_dir}/local/autodock_vina_1_1_2_linux_x86/bin/vina',
+                    '--config', f'{home_dir}/vina_docking/data/conf/conf_{args.target}.txt','--exhaustiveness', f'{args.ex}', 
                     '--log', 'tmp/log.txt'])
         end = time()
         print("Docking time :", end-start)
