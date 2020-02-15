e333966b57534461c4737c1b20ade4c848a330eb
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-02-15 12:32:54 -0500

    Update main_dock_smi.py

--- main_dock_smi.py
+++ main_dock_smi.py
@@ -42,6 +42,7 @@ def main(args):
     # Runs the docking process with the args provided
     
     home_dir='/home/jboitr/projects/def-jeromew/jboitr'
+    install_dir = '/home/jboitr/projects/def-jeromew/docking_install'
     
     # Uncomment to Copy receptor file from the DUDE dir if first time using this target. 
     #shutil.copyfile(f'/home/mcb/users/jboitr/data/all/{args.target}/receptor.pdb',f'data/receptors/{args.target}.pdb')
@@ -50,7 +51,7 @@ def main(args):
     
     # target to pdbqt 
     subprocess.run(['python3','pdb_select.py',f'{receptor_filepath}','! hydro', f'{receptor_filepath}'])
-    subprocess.run([f'{home_dir}/local/mgltools_x86_64Linux2_1.5.6/bin/pythonsh', 'prepare_receptor4.py',
+    subprocess.run([f'{install_dir}/mgltools_x86_64Linux2_1.5.6/bin/pythonsh', 'prepare_receptor4.py',
                     f'-r {home_dir}/vina_docking/{receptor_filepath}','-o tmp/receptor.pdbqt', '-A hydrogens'])
     
     # Iterate on molecules
@@ -71,12 +72,12 @@ def main(args):
             f.close()
         
         # ligand mol2 to pdbqt 
-        subprocess.run([f'{home_dir}/local/mgltools_x86_64Linux2_1.5.6/bin/pythonsh', 'prepare_ligand4.py',
+        subprocess.run([f'{install_dir}/mgltools_x86_64Linux2_1.5.6/bin/pythonsh', 'prepare_ligand4.py',
                         f'-l tmp/ligand.mol2', '-o tmp/ligand.pdbqt', '-A hydrogens'])
         
         # RUN DOCKING 
         start=time()
-        subprocess.run([f'{home_dir}/local/autodock_vina_1_1_2_linux_x86/bin/vina',
+        subprocess.run([f'{install_dir}/autodock_vina_1_1_2_linux_x86/bin/vina',
                     '--config', f'{home_dir}/vina_docking/data/conf/conf_{args.target}.txt','--exhaustiveness', f'{args.ex}', 
                     '--log', 'tmp/log.txt'])
         end = time()
