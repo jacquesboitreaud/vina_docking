567f267db86d4212d51cf3daf0a2b48ad255ec6b
Jacques Boitreaud <jboitr@cedar5.cedar.computecanada.ca>
2020-02-19 14:24:39 -0800

    works on ccanada

--- job_array.sh
+++ job_array.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-jeromew
 #SBATCH --array 1-10
-#SBATCH --time=24:00:00
-#SBATCH --job-name=v_screen
-#SBATCH --output=%x_$SLURM_ARRAY_TASK_ID.out
-python3 dock_smi.py -t drd3 -df data/split/mol_batch_$SLURM_ARRAY_TASK_ID.csv -o data/scored/scored_$SLURM_ARRAY_TASK_ID.csv
+#SBATCH --time=72:00:00
+#SBATCH --job-name=virtual_screen
+#SBATCH --output=out/%x_${SLURM_ARRAY_JOB_ID}.out
+echo "Starting task $SLURM_ARRAY_TASK_ID"
+python3 dock_df.py -t drd3 -df data/split/split_batch_${SLURM_ARRAY_TASK_ID}.csv -o data/scored/scored_${SLURM_ARRAY_TASK_ID}.csv -s cedar
